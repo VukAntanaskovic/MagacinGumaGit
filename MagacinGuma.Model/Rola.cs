399bf5e4a8604a3cddbe2e404595f149c3e91a6c
Miloš Đurić <djusilord@gmail.com>
2022-02-13 19:20:25 +0100

    Unos novog korisnika

---
+++ MagacinGuma.Model/Rola.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagacinGuma.Model
+{
+    public class Rola
+    {
+        public int RolaId { get; set; }
+        public string RolazNaziv { get; set; }
+    }
+}
